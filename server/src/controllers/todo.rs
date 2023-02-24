53c94e7eb9e0a4ca6c338712970fcd7a86603c3f
Irwin Arruda <arruda.irwin@gmail.com>
2023-02-23 23:57:04 -0300

    Added new error handling for the route

--- server/src/controllers/todo.rs
+++ server/src/controllers/todo.rs
@@ -5,10 +5,11 @@ use crate::repositories::todo::*;
 #[get("/todos")]
 pub async fn get_todos() -> impl Responder {
     let todos_repository = TodoRepository {};
-    return match todos_repository.get_todos() {
-        Ok(todos) => HttpResponse::Ok()
-            .content_type(ContentType::json())
-            .body(serde_json::to_string(&todos).unwrap()),
-        Err(_) => HttpResponse::InternalServerError().body(""),
-    };
+    let todos = todos_repository
+        .get_todos()
+        .map_err(|_| HttpResponse::InternalServerError().body(""))
+        .unwrap();
+    return HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(serde_json::to_string(&todos).unwrap());
 }
